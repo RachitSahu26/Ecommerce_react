bdca3f28ae6b23b553f8fae392a3643acb8881d0
rachit <curiousrachit.26@gmail.com>
2023-07-25 13:53:44 +0530

    link add

--- Ecommerce/src/Component/Footer.jsx
+++ Ecommerce/src/Component/Footer.jsx
@@ -7,7 +7,7 @@ export default function () {
 <div className="link-area">
 <a href="https://github.com/RachitSahu26" ><img src="https://www.freepnglogos.com/uploads/512x512-logo/512x512-transparent-logo-github-logo-24.png" width="70" alt="512x512 transparent logo, github logo" className='github'/></a>
 <a href="https://www.linkedin.com/in/rachit-sahu-004ba2255/" ><img src="https://www.freepnglogos.com/uploads/linkedin-basic-round-social-logo-png-13.png" width="63" alt="linkedin basic round social logo png" className='linkdin'/></a>
-<a href="" ><img src="https://www.freepnglogos.com/uploads/chain-png/supply-chain-transformation-accenture-22.png" width="70" alt="supply chain transformation accenture" className='link'/></a>
+<a href="https://github.com/RachitSahu26/Ecommerce_react" ><img src="https://www.freepnglogos.com/uploads/chain-png/supply-chain-transformation-accenture-22.png" width="70" alt="supply chain transformation accenture" className='link'/></a>
 </div>
 
 
